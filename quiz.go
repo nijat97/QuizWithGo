2de332a13402c7789851e2fcb41781f084d0f8fa
ehasnij <nijat.hasanov@ericsson.com>
2022-09-05 10:52:48 +0200

    Added shuffle flag for questions
    Added trimming white spaces from the answer

--- quiz.go
+++ quiz.go
@@ -4,49 +4,58 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
+	"math/rand"
 	"os"
-	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	correct, total := 0, 0
+	rand.Seed(time.Now().UnixNano())
+
 	file := flag.String("file", "problems.csv", "file for problems")
 	t := flag.Int("time", 30, "timer for game")
-
+	sh := flag.Bool("shuffle", false, "shuffle the questions")
 	flag.Parse()
+
 	f, err := os.Open(*file)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+
 	csv := csv.NewReader(f)
+	records, err := csv.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *sh == true {
+		rand.Shuffle(len(records), func(i, j int) { records[i], records[j] = records[j], records[i] })
+	}
 	fmt.Println("Press Enter to start time.")
 	fmt.Scanln()
 
 	timer := time.NewTimer(time.Duration(*t) * time.Second)
 	go func() {
 		<-timer.C
-		fmt.Printf("Correct answers: %d out of %d\n", correct, total)
+		fmt.Printf("\nCorrect answers: %d out of %d\n", correct, total)
 		os.Exit(0)
 	}()
-	for {
-		ans := 0
-		record, err := csv.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
+
+	for _, rec := range records {
+		ans := ""
 		total++
-		fmt.Printf("Question: %s\n", record[0])
+		fmt.Printf("Question: %s\n", rec[0])
 		fmt.Scanln(&ans)
-		corr, err := strconv.Atoi(record[1])
-		if corr == ans {
+
+		ans = strings.TrimSpace(ans)
+		if ans == rec[1] {
 			correct++
 			fmt.Println("Correct!")
 		}
 	}
+	fmt.Printf("\nCorrect answers: %d out of %d\n", correct, total)
 }
